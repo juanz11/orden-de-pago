86bedfe5c04e55055d7c94da0f5be107b83ead11
Juan Zamora <uraharazamora@gmail.com>
2025-04-01 10:07:25 -0400

    asistencia with

--- resources/views/suppliers/index.blade.php
+++ resources/views/suppliers/index.blade.php
@@ -19,7 +19,9 @@
                 @endif
 
                 <div class="overflow-x-auto">
-                    <table class="min-w-full divide-y divide-gray-200">
+                    <table class="min-w-full divide-y divide-gray-200" style="
+    width: 100%;
+">
                         <thead class="bg-gray-50">
                             <tr>
                                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nombre</th>
